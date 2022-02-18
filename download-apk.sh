e7522ecb4abb6b012887b35253fc3d2e269d5dc8
Azzam Nizar <15.azzamn@xl.co.id>
2022-02-18 20:43:10 +0700

    Fixing shell script

--- download-apk.sh
+++ download-apk.sh
@@ -1,2 +1,3 @@
 wget https://github.com/flutter/gallery/releases/download/v2.9.2/flutter_gallery_Android.apk
-mv flutter_gallery_Android.apk src/test/resources/apk
+mkdir src/test/resources/apk/
+mv flutter_gallery_Android.apk src/test/resources/apk/
